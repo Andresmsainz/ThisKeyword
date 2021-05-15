edf9f963c82aab11d4b137f1d703d0eda22b44eb
Andres Sainz <andresmsainz@gmail.com>
2021-05-14 21:36:15 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ThisKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This Keyword!");
+
+            Person person = new Person();
+            person.DisplayName();
+            person.DisplayName("Don","Johnson");
+        }
+    }
+
+    class Person
+    {
+        string firstName;
+        string lastName;
+
+        public void DisplayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+            
+        }
+
+        public void DisplayName(string firstName, string lastName)
+        {
+            Console.WriteLine(firstName + " " + lastName); //retrieves values passed
+            Console.WriteLine(this.firstName + " " + this.lastName); //retrieves values from this class
+        }
+
+        public Person()
+        {
+            firstName = "Not Yet Determined";
+            lastName = string.Empty;
+        }
+            
+
+    }
+}
